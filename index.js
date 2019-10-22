a61ffa5ea63b977c6f1fc48c1ca8ddafa523b3d4
Rise Erpelding <rise.erpelding@gmail.com>
2019-10-22 10:27:32 -0500

    submit button working

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-"use strict";
+"USE STRICT";
 
 //These variables keep track of the question index and number
 let questionIndex = 0;
 let questionNumber = questionIndex + 1;
+let numberCorrect = 0;
 
 function handleQuestion() {
-    //This function will be responsible for displaying the question, multiple choice answers, and example
+    //This function will be responsible for displaying the question, multiple choice answers, and example, and submitting the answer
     $('#content-box').on('click', `.begin`, function() {
         $('.quiz-description').remove();
         $('.begin').remove();
         const question = displayQuestion(STORE);
         $('#content-box').html(question);
+        submitAnswer();
         console.log('`handleQuestion` ran');
     });
+
 
 }
 
 function submitAnswer() {
     //This function will be responsible for submitting the answer
     $('#question-form').submit(function(event) {
-        console.log("button is working");
         event.preventDefault();
+        console.log("submit button is working");
+        // Delete 2 lines below
         // $('.quiz-description').remove();
         // $('.begin').remove();
         displayFeedback();
@@ -28,9 +32,11 @@ function submitAnswer() {
     console.log('`submitAnswer` ran');
 }
 
+
 function displayQuestion(item) {
     //This function is responsible for displaying the question each time
     console.log("Displaying the question");
+    showQuestionNumber();//potentially move this? should it be in displayQuestion or handleQuestion above submitAnswer? Or does it matter?
     return `
     <section class="question-box">
     <p class="instructions content">
@@ -44,34 +50,48 @@ function displayQuestion(item) {
     <form id="question-form">
         <ul class="answers content">
             <li>
-                <input type="radio" id="answerA" name="answer" value="a"><label for="answerA">  ${item[questionIndex]["answerOptions"][0]}</label>
+                <input type="radio" id="answerA" name="answer" value="${item[questionIndex]["answerOptions"][0]}"><label for="answerA">  ${item[questionIndex]["answerOptions"][0]}</label>
             </li>
             <li>
-                <input type="radio" id="answerB" name="answer" value="b"><label for="answerB">   ${item[questionIndex]["answerOptions"][1]}</label>
+                <input type="radio" id="answerB" name="answer" value="${item[questionIndex]["answerOptions"][1]}"><label for="answerB">   ${item[questionIndex]["answerOptions"][1]}</label>
             </li>
             <li>
-                <input type="radio" id="answerC" name="answer" value="c"><label for="answerC">  ${item[questionIndex]["answerOptions"][2]}</label>
+                <input type="radio" id="answerC" name="answer" value="${item[questionIndex]["answerOptions"][2]}"><label for="answerC">  ${item[questionIndex]["answerOptions"][2]}</label>
             </li>
             <li>
-            <input type="radio" id="answerD" name="answer" value="d"><label for="answerD">  ${item[questionIndex]["answerOptions"][3]}</label>
+            <input type="radio" id="answerD" name="answer" value="${item[questionIndex]["answerOptions"][3]}"><label for="answerD">  ${item[questionIndex]["answerOptions"][3]}</label>
             </li>
         </ul>
-        <button type="submit" class="retrieve-feedback">submit</button>
+        <input type="submit" class="sbmt"></input>
     </form>
     </section>`;
+    //Note to self: this is messy to look at, find a way to clean up later
 }
 
+function correctFeedback() {
+    $('#content-box').html(`<p>You picked the correct answer</p>`);
+    console.log('`correctFeedback` ran');
+}
+
+function incorrectFeedback() {
+    $('#content-box').html(`<p>You picked the wrong answer</p>`);
+    console.log('`incorrectFeedback` ran');
+}
 
 function displayFeedback() {
     //This function will be responsible for handling whether the feedback for the correct answer or the feedback for the incorrect answer is displayed
-        // let selectedAnswer = $('input:checked');
-        // let correctAnswer = STORE[questionIndex]["correctAnswer"];
-        // if (selectedAnswer === correctAnswer) {
-        //     $('#content-box').html(`<p>Hello world</p>`)
-        // }
-    $('#content-box').html(`<p>Feedback would be displayed here.</p>`)
+        let selectedAnswer = $('input:checked').val();
+        let correctAnswer = STORE[questionIndex]["correctAnswer"];
+
+        if (selectedAnswer === correctAnswer) {
+            correctFeedback();
+        } else {
+            incorrectFeedback();
+        }
     console.log('`displayFeedback` ran');
 }
+
+
 
 function changeScore() {
     //This function will be responsible for changing the number of correct answers in the header
@@ -80,6 +100,7 @@ function changeScore() {
 
 function showQuestionNumber() {
     //This function will be responsible for changing the number of the question in the header
+    $('.question-number').replaceWith(questionNumber);
     console.log('`showQuestionNumber` ran');
 }
 
@@ -91,7 +112,6 @@ function endQuiz() {
 function handleQuiz() {
 
     handleQuestion();
-    submitAnswer();
     changeScore();
     showQuestionNumber();
     endQuiz();
--- store.js
+++ store.js
@@ -38,5 +38,165 @@ const STORE = [
 
         exampleSource: 
         'A Game of Thrones'
+    },
+    {
+        //Question 3
+        question: 'dissonance',
+
+        answerOptions: [
+        'a close parallel or repetition of an idea, feeling, style, or event',
+        'a loud and confused noise, especially that of people shouting vehemently',
+        'a tension or clash resulting from the combination of two disharmonious or unsuitable elements',
+        'the condition of being far off; remoteness'
+        ],
+
+        correctAnswer:
+        'a tension or clash resulting from the combination of two disharmonious or unsuitable elements',
+   
+        exampleSentence:
+        '“t’s doubtful that watching Nancy Drew get busy will be a deal-breaker for most people; a slap of cognitive dissonance for some, definitely.”',
+
+        exampleSource: 
+        'Salon, October 8, 2019'
+    },
+    {
+        //Question 4
+        question: 'aberration',
+
+        answerOptions: [
+        'a departure from what is normal, usual, or expected, typically one that is unwelcome',
+        'cruel and bitter criticism',
+        'offense or annoyance',
+        'the action or fact of making a specified thing smaller or less in amount, degree, or size'
+        ],
+
+        correctAnswer:
+        'a departure from what is normal, usual, or expected, typically one that is unwelcome',
+   
+        exampleSentence:
+        '“Supreme Court Justice Ruth Bader Ginsburg said Thursday that the current political climate will be seen as “an aberration” in U.S. history.”',
+
+        exampleSource: 
+        'Washington Times, October 3, 2019'
+    },
+    {
+        //Question 5
+        question: 'vitriol',
+
+        answerOptions: [
+        'a departure from what is normal, usual, or expected, typically one that is unwelcome',
+        'a tension or clash resulting from the combination of two disharmonious or unsuitable elements',
+        'cruel and bitter criticism',
+        'the action or fact of making a specified thing smaller or less in amount, degree, or size'
+        ],
+
+        correctAnswer:
+        'cruel and bitter criticism',
+   
+        exampleSentence:
+        '“That isn’t an unfair or unlikely view of the world, but it’s a simplified one that ignores more significant issues, like virtual harassment and online vitriol.”',
+
+        exampleSource: 
+        'The Verge, October 10, 2019'
+    },
+    {
+        //Question 6
+        question: 'umbrage',
+
+        answerOptions: [
+        'a close parallel or repetition of an idea, feeling, style, or event',
+        'a departure from what is normal, usual, or expected',
+        'offense or annoyance',
+        'the presentation of something as being smaller, worse, or less important than it actually is'
+        ],
+
+        correctAnswer:
+        'offense or annoyance',
+   
+        exampleSentence:
+        '“It seems that the chef she spoke with took umbrage not just with the request for a special, non-menu item, but also that they not be charged full price for the meal.”',
+
+        exampleSource: 
+        'Fox News, September 21, 2019'
+    },
+    {
+        //Question 7
+        question: 'tyro',
+
+        answerOptions: [
+        'a beginner or novice',
+        'a person exercising power or control in a cruel, unreasonable, or arbitrary way',
+        'a person who dislikes humankind and avoids human society',
+        'a person who seeks to promote the welfare of others'
+        ],
+
+        correctAnswer:
+        'a beginner or novice',
+   
+        exampleSentence:
+        '“The first when I was a young tyro cutting my peripatetic teeth; the latter just 18 months ago, when a confirmed flounderer in the murky depths of middle age.”',
+
+        exampleSource: 
+        'The Guardian, June 21, 2019'
+    },
+    {
+        //Question 8
+        question: 'polemic',
+
+        answerOptions: [
+        'a departure from what is normal, usual, or expected, typically one that is unwelcome',
+        'a strong verbal or written attack on someone or something',
+        'a tension or clash resulting from the combination of two disharmonious or unsuitable elements',
+        'the action or fact of making a specified thing smaller or less in amount, degree, or size'
+        ],
+
+        correctAnswer:
+        'a strong verbal or written attack on someone or something',
+   
+        exampleSentence:
+        '“Bloom’s cultural polemics were sharp-tongued and colourful, but his smile was warm and boyish.”',
+
+        exampleSource: 
+        'The Guardian, October 14, 2019'
+    },
+    {
+        //Question 9
+        question: 'precipitate',
+
+        answerOptions: [
+        'bring about abruptly',
+        'dispute the truth, validity, or honesty of (a statement or motive); call into question',
+        'make (something) ready for use or consideration',
+        'speak or act in an evasive way'
+        ],
+
+        correctAnswer:
+        'bring about abruptly',
+   
+        exampleSentence:
+        '“Davis describes the aftermath of his Paris tour in 1949 and 1950—the anguish of his return to the relentless racism of the United States—as precipitating his heroin addiction.”',
+
+        exampleSource: 
+        'The New Yorker, August 28, 2019'
+    },
+    {
+        //Question 10
+        question: 'vacillate',
+
+        answerOptions: [
+        'alternate or waver between different opinions or actions; be indecisive',
+        'cause or give rise to (a feeling, situation, or condition)',
+        'cause (someone) to feel drained of energy or vitality; weaken',
+        'speak or act in an evasive way'
+        ],
+
+        correctAnswer:
+        'alternate or waver between different opinions or actions; be indecisive',
+   
+        exampleSentence:
+        '“The main opposition Labour Party is itself deeply split over Brexit, and appears to be vacillating about whether to launch a motion of no-confidence to try and bring Johnson down.”',
+
+        exampleSource: 
+        'Reuters, September 24, 2019'
     }
 ]
